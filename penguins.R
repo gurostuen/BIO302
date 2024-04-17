05862e5e8898beb1989dc6759c70dc6376117f8c
Guro Ødegårdstuen <guro.odegardstuen@gmail.com>
2024-04-17 14:10:19 +0200

    Penguin data

---
+++ penguins.R
@@ -0,0 +1,10 @@
+library(palmerpenguins)
+library(tidyverse)
+penguins
+
+p <- penguins |> 
+  drop_na(sex) |> 
+  group_by(species, sex) |> 
+  summarise(
+    bill_length_mean = mean(bill_length_mm),
+    .groups = "drop")
