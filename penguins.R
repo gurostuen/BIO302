b0fd1bd20b89dae1b47d7f0a25a3ca30aba51880
Guro Ødegårdstuen <guro.odegardstuen@gmail.com>
2024-04-17 14:40:12 +0200

    Update

--- penguins.R
+++ penguins.R
@@ -4,6 +4,14 @@ penguins
 
 p <- penguins |> 
   drop_na(sex) |> 
-  group_by(species, sex) 
+  group_by(species, sex) |> 
+  summarise(
+    bill_length_mean = mean(bill_length_mm),
+    .groups = "drop")
 
-p
+penguins |> filter(sex == "male", species == "Gentoo")
+
+penguins
+
+
+
